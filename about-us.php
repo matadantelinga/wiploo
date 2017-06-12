873ef72c825e83c01f07d87bf7c29de0b0f6eaa1
[matadantelinga] <[indrayanasoetedja@gmail.com]>
2017-06-12 16:46:34 +0700

    about page

---
+++ about-us.php
@@ -0,0 +1,55 @@
+<?php include"header.php"; ?>
+        <?php include 'static-banner.php'; ?>
+
+        <section id="one-column-template" class="vertical-padding">
+           <div class="container">
+                <div class="col-md-12">
+                    <div class="section-title-container">
+                        <h4 class="center-section-title header-font">About Us</h4>
+                        <hr class="center-red-line-bg">
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <p>Lorem ipsum, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                </div>
+                <div class="col-md-12">
+                    <div class="section-title-container">
+                        <h4 class="center-section-title header-font">Get In Touch</h4>
+                        <hr class="center-red-line-bg">
+                    </div>
+                </div>
+                 <div class="col-md-6 col-sm-6">
+                    <p>If you have something to share with us, dont forget to leave your words by filling our contact form bellow, we'll make an appointment and meet so soon.</p>
+                    <form id="contact-form">
+                        <div class="form-wrapper vertical-padding">
+                            <input type="text" name="name" placeholder="Your Name" class="width-100 ">
+                        </div>
+                        <div class="form-wrapper">
+                            <input type="email" name="name" placeholder="Your Email" class="width-100">
+                        </div>
+                        <div class="form-wrapper vertical-padding">
+                            <textarea rows="10" class="width-100" ></textarea>
+                        </div>
+                    </form>
+                </div>
+                <div class="col-md-6 col-sm-6">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.704936102214!2d106.89296721477686!3d-6.302445895438659!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69ed50d076a261%3A0x73127b9ecf2da0c!2sTaman+Mini+Indonesia+Indah!5e0!3m2!1sid!2sid!4v1497257842461" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                </div>
+                
+           </div>
+        </section>
+
+        
+           
+           
+
+
+            <!--
+            ==================================================
+            Call To Action Section Start
+            ================================================== -->
+            <section id="cta-container" class="vertical-padding dark-green-bg">
+                <?php include"cta-content.php"; ?>
+            </section>  
+
+    <?php include"footer.php"; ?>
---
+++ static-banner.php
@@ -0,0 +1,3 @@
+<section id="static-banner-container">
+	<img src="assets/images/aboutbanner.jpg" class="width-100" />
+</section>
